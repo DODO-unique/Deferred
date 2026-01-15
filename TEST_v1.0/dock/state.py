41ff7aaf30db3dfec2cece28d78c8db012101550
V. Prescott <victorprescott554@gmail.com>
2026-01-15 15:19:11 +0530

    added purpose. The file is correct. Changed dictionary typing

--- TEST_v1.0/dock/state.py
+++ TEST_v1.0/dock/state.py
@@ -1,4 +1,6 @@
-TASKS: dict[str, dict] = {}
+# I will be frank why I made this. I wanted to use TASKS in two different functions/routes/decorators. I thought I could just go self.TASKS and wrap everything there in a class but then that felt complicated so I said 'nope' and well, we are here.
+
+TASKS: dict[str, str] = {}
 
 def clear_tasks():
     TASKS.clear()
