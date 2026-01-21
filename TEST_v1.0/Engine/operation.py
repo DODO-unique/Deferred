49d0f1c387e255c6e8cef25c2a361dcd64ffd4fb
V. Prescott <victorprescott554@gmail.com>
2026-01-21 09:46:57 +0530

    testing remaining. Theoretically works

--- TEST_v1.0/Engine/operation.py
+++ TEST_v1.0/Engine/operation.py
@@ -4,18 +4,56 @@ Docstring for TEST_v1.0.Engine.operation
 Goal is to define what each flag should do in a certain type.
 '''
 
+from uuid import uuid4
+from datetime import datetime
+from ..Utility.pydantic_models import createInitiation, Versions
+from ..Utility.logger import loggy
+
+def log(msg: str):
+    loggy(local="Engine/operation")
+
 # we will frist take the create flag onlys
-def create(cat: str):
-    '''
-    Docstring for create: handles all create stages and operations, and state
+class create:
+
+    def __init__(self):
+        self.state = createInitiation(
+            op_id="",
+            next_state="Not Set",
+            completed_steps={},
+            version=Versions(version="Tv_1.0")
+        )
     
-    :param cat: sanitized, validated string of category
-    :type cat: str, with all caps through pydantic super validation in docking layer
-    '''
+    def executing_filter(self, cat):
+        if cat == 'init':
+            if self.state.next_state == 'Not Set':
+                log(
+                    "reached initiation state. Forwarding control to _initiation."
+                )
+                instructions = self.initiation()
+                log(
+                    "returning instructions (1)"
+                )
+                return instructions
 
-    if cat == 'init':
-        # initiation signal, this means we have to create a op_state. We have to make sure the given id does not take any other signals now. Only what we request.
-        # we will do that by making sure we make it obey operation_id op_id. 
-        # when defining state, we have to make sure we are defining an op_id, this op_id is required to identify state.
-        # the op_id should be global, so we should have a state where op_id is tracked.
 
+    def _initiation(self):
+        # generate op_id
+        uid = uuid4()
+
+        # store it, change self.state and return the instructions 
+        self.state.op_id = uid
+        self.state.next_state = 'context'
+        self.state.completed_steps['phases_completed'] = 'init'
+        self.state.completed_steps['timestamp'] = datetime.now().isoformat()
+        self.state.version = "Tv_1.0"
+
+        instructions = {
+            "payload" : True,
+            "time" : True,
+            "creation_time" : True,
+            "op_id" : True,
+            "version" : True
+        }
+
+        return instructions
+    
