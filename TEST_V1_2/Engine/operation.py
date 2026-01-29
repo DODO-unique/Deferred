1750776816c0768266307e013ee8397cf8c45e53
V. Prescott <victorprescott554@gmail.com>
2026-01-29 17:37:18 +0530

    First upload of this file, would need some pylance error handling

---
+++ TEST_V1_2/Engine/operation.py
@@ -0,0 +1,61 @@
+'''
+Docstring for TEST_v1.0.Engine.operation
+
+Goal is to define what each flag should do in a certain type.
+'''
+
+from uuid import uuid4
+from datetime import datetime
+from Utility.pydantic_models import createInitiation, Versions, Category
+from Utility.logger import loggy
+
+def log(msg: str):
+    loggy(local="Engine/operation", log=msg)
+
+# we will frist take the create flag onlys
+class create:
+
+    def __init__(self):
+        self.state = createInitiation(
+            op_id = None,
+            next_state = "Not Set",
+            completed_steps = {},
+            version = Versions(version="Tv_1.0")
+        )
+    
+    def executing_filter(self, cat: Category) -> dict[str, bool]:
+        if cat.value == 'init':
+            if self.state.next_state == 'Not Set':
+                log(
+                    "reached initiation state. Forwarding control to _initiation."
+                )
+                instructions = self._initiation()
+                log(
+                    "returning instructions (1)"
+                )
+                return instructions
+        
+        raise ValueError("Deferred: Can not execute filter for category")
+
+
+    def _initiation(self):
+        # generate op_id
+        uid = uuid4()
+
+        # store it, change self.state and return the instructions 
+        self.state.op_id = uid
+        self.state.next_state = 'context'
+        self.state.completed_steps['phases_completed'] = 'init'
+        self.state.completed_steps['timestamp'] = datetime.now().isoformat()
+        self.state.version.version = "Tv_1.0"
+
+        instructions = {
+            "payload" : True,
+            "time" : True,
+            "creation_time" : True,
+            "op_id" : True,
+            "version" : True
+        }
+
+        return instructions
+    
