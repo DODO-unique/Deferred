3e5c2c30c8377b14f146c93c5014ca94d7994104
V. Prescott <victorprescott554@gmail.com>
2026-03-21 20:26:27 +0530

    fix: handle None

--- TEST_V3_X/TEST_V3_1/Touch/ORM_2.py
+++ TEST_V3_X/TEST_V3_1/Touch/ORM_2.py
@@ -20,12 +20,14 @@ async def fetch_line(uname: UserName) -> Users | None:
     except Exception as e:
         initiate_error_handler(message="could not fetch user", errCode=ErrorCodes.BROAD_DATABASE_ERROR.value, error=e)
 
-async def new_session(session_token: UUID, user: Users) -> bool | None:
+async def new_session(session_token: UUID, user: Users | None) -> bool | None:
     try:
+        if user is None:
+            initiate_error_handler(message="user not found", errCode=ErrorCodes.USER_NOT_FOUND.value, error=ValueError("user not found"))
         async with AsyncSession(engine) as session:
             new_session_entry = Users(token = session_token, user_id = user)
             session.add(new_session_entry)
             await session.commit()
             return True
     except Exception as e:
-        initiate_error_handler(message="could not create session", errCode=ErrorCodes.BROAD_DATABASE_ERROR.value, error=e)
+        initiate_error_handler(message="could not create session", errCode=ErrorCodes.CANNOT_CREATE_SESSION.value, error=e)
