bb0b89364c828cba0cb828599ff96e9b68281f92
V. Prescott <victorprescott554@gmail.com>
2026-03-21 20:02:24 +0530

    feat: new session entry creation

--- TEST_V3_X/TEST_V3_1/Touch/ORM_2.py
+++ TEST_V3_X/TEST_V3_1/Touch/ORM_2.py
@@ -3,6 +3,7 @@ from Touch.orm_schema import Users
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from Utility.ErrorHandler import initiate_error_handler, ErrorCodes
+from uuid import UUID
 
 URL = "postgresql+asyncpg://victor:yomama@localhost:5506/deferred"
 engine = create_async_engine(URL)
@@ -18,3 +19,13 @@ async def fetch_line(uname: UserName) -> Users | None:
             return line_object.scalar_one_or_none()
     except Exception as e:
         initiate_error_handler(message="could not fetch user", errCode=ErrorCodes.BROAD_DATABASE_ERROR.value, error=e)
+
+async def new_session(session_token: UUID, user: Users) -> bool | None:
+    try:
+        async with AsyncSession(engine) as session:
+            new_session_entry = Users(token = session_token, user_id = user)
+            session.add(new_session_entry)
+            await session.commit()
+            return True
+    except Exception as e:
+        initiate_error_handler(message="could not create session", errCode=ErrorCodes.BROAD_DATABASE_ERROR.value, error=e)
